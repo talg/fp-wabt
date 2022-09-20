edfeda123bd2b342e20a3132acd44075e57ef9d2
Tal Garfinkel <talg@cs.stanford.edu>
2022-09-20 14:52:28 -0700

    cleanup cruft in uvwasi-rt-main.c

--- wasm2c/examples/hello-wasi/uvwasi-rt-main.c
+++ wasm2c/examples/hello-wasi/uvwasi-rt-main.c
@@ -20,59 +20,27 @@ int main(int argc, const char** argv)
 {
 
     uvwasi_options_t init_options;
-    /*
-    #define ENV_COUNT       7
-    #define PREOPENS_COUNT  2
 
-    char* env[ENV_COUNT];
-    env[0] = "TERM=xterm-256color";
-    env[1] = "COLORTERM=truecolor";
-    env[2] = "LANG=en_US.UTF-8";
-    env[3] = "PWD=/";
-    env[4] = "HOME=/";
-    env[5] = "PATH=/";
-    env[6] = NULL;
-
-    uvwasi_preopen_t preopens[PREOPENS_COUNT];
-
-    //No sandboxing is enforced, binary has access to everything user does
-
-    preopens[0].mapped_path = "/";
-    preopens[0].real_path = "/";
-    preopens[1].mapped_path = "./";
-    preopens[1].real_path = ".";
-
-    init_options.argc = argc;
-    init_options.argv = argv;
-    init_options.envp = (const char **) env;
-    init_options.preopenc = PREOPENS_COUNT;
-    init_options.preopens = preopens;
-    */
-
+    //pass in standard descriptors
     init_options.in = 0;
     init_options.out = 1;
     init_options.err = 2;
     init_options.fd_table_size = 3;
-    /* init_options.argc = 3; */
-    /* init_options.argv = calloc(3, sizeof(char*)); */
-    /* init_options.argv[0] = "--foo=bar"; */
-    /* init_options.argv[1] = "-baz"; */
-    /* init_options.argv[2] = "100"; */
-    /* init_options.envp = NULL; */
+
+    //pass in args and environement
     extern const char ** environ;
     init_options.argc = argc;
     init_options.argv = argv;
     init_options.envp = (const char **) environ;
 
+    //no sandboxing enforced, binary has access to everything user does
     init_options.preopenc = 2;
     init_options.preopens = calloc(2, sizeof(uvwasi_preopen_t));
 
-    //no sandboxing enforced, binary has access to everything user does
     init_options.preopens[0].mapped_path = "/";
     init_options.preopens[0].real_path = "/";
     init_options.preopens[1].mapped_path = "./";
     init_options.preopens[1].real_path = ".";
-
 
     init_options.allocator = NULL;
 
